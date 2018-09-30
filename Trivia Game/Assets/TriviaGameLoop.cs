17affbb7b6fc58ebbc708dbe4cb137b6cd2ad42b
TommyCollins93 <dominom007@gmail.com>
2018-10-01 01:14:37 +0200

    Adding TriviaGame
    Adding TriviaGame Unity tutorial

---
+++ Trivia Game/Assets/TriviaGameLoop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriviaGameLoop : MonoBehaviour {
+
+	public struct Question
+	{
+		public string questionText;
+		public string[] answers;
+		public int correctAnswerIndex;
+		public Question(string questionText, string[] answers, int correctAnswerIndex)
+		{
+			this.questionText = questionText;
+			this.answers = answers;
+			this.correctAnswerIndex = correctAnswerIndex;
+		}
+	}
+	public Question currentQuestion = new Question("What is your favourite color?", new string[]{"blue","red","yelow","white","black"}, 0);
+	public Button[] answerButtons;
+	public Text questionText;
+
+	private Question[] questions;
+
+	// Use this for initialization
+	void Start () {
+		assignQuestion();
+	}
+	
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void assignQuestion(){
+
+		questionText.text = currentQuestion.questionText;
+		for (int i = 0; i < answerButtons.Length; i++){
+			answerButtons[i].GetComponentInChildren<Text>().text = currentQuestion.answers[i];
+		}
+
+	}
+
+	public void checkAnswer(int buttonNum){
+		if (buttonNum == currentQuestion.correctAnswerIndex){
+			print("Correct.");
+		}
+		else{
+			print("Incorrect.");
+		}
+	}
+}
---
+++ Trivia Game/Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	public int age;
+	public float height;
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+		print(age + " " + height + " " + name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
